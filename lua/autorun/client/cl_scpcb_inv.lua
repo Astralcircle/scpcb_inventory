c9f4c1048d94d9529ee7c43eb1f97672244757e0
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-06-19 15:31:10 +0300

    first commit

---
+++ lua/autorun/client/cl_scpcb_inv.lua
@@ -0,0 +1,265 @@
+local inventory = {}
+local icons_parent
+local menublack = Material("scpcb/menublack.png", "noclamp smooth")
+local menuwhite = Material("scpcb/menuwhite.png", "noclamp smooth")
+local menuscale = ScrH() / 1024
+
+surface.CreateFont("SCPCB_Inventory", {
+	font = "Courier New",
+	size = 19 * menuscale,
+	extended = true,
+	shadow = true
+})
+
+if IsValid(icons_parent) then
+	icons_parent:Remove()
+end
+
+hook.Add("OnScreenSizeChanged", "G", function()
+	menuscale = ScrH() / 1024
+
+	surface.CreateFont("SCPCB_Inventory", {
+		font = "Courier New",
+		size = 19 * menuscale,
+		extended = true,
+		shadow = true
+	})
+end)
+
+local function GetClassIcon(class)
+	if file.Exists("materials/entities/" .. class .. ".png", "GAME") then
+		return "entities/" .. class .. ".png"
+	end
+
+	if file.Exists("materials/vgui/entities/" .. class .. ".vtf", "GAME") then
+		return "vgui/entities/" .. class
+	end
+
+	if file.Exists("materials/vgui/entities/" .. class .. ".vmt", "GAME") then
+		return "vgui/entities/" .. class
+	end
+
+	local swep = weapons.GetStored(class)
+
+	if swep and swep.IconOverride then
+		return swep.IconOverride
+	end
+
+	local ent = scripted_ents.GetStored(class)
+
+	if ent and ent.IconOverride then
+		return ent.IconOverride
+	end
+end
+
+local function GetClassName(class)
+	local name = language.GetPhrase(class)
+
+	if name ~= class then
+		return name
+	end
+
+	local swep = weapons.GetStored(class)
+
+	if swep and swep.PrintName then
+		return swep.PrintName
+	end
+
+	local ent = scripted_ents.GetStored(class)
+
+	if ent and ent.PrintName then
+		return ent.PrintName
+	end
+
+	return "???"
+end
+
+hook.Add("HUDShouldDraw", "SCPCB_HideHUD", function(name)
+	if name == "CHudWeaponSelection" then
+		return false
+	end
+
+	if name == "CHudCrosshair" and IsValid(icons_parent) then
+		return false
+	end
+end)
+
+hook.Add("PostGamemodeLoaded", "SCPCB_HideHUD", function()
+	function GAMEMODE:HUDWeaponPickedUp()
+
+	end
+end)
+
+hook.Add("PlayerButtonDown", "SCPCB_OpenInventory", function(ply, button)
+	if button ~= KEY_G then
+		return
+	end
+
+	if IsValid(icons_parent) then
+		icons_parent:Remove()
+		return
+	end
+
+	local slot_size, spacing = 70 * menuscale, 35 * menuscale
+	local rows, cols = 2, 5
+
+	local local_player = LocalPlayer()
+	local total_width = cols * slot_size + (cols - 1) * spacing
+	local total_height = rows * slot_size + (rows - 1) * slot_size
+
+	icons_parent = vgui.Create("Panel")
+	icons_parent:SetSize(total_width, total_height)
+	icons_parent:MakePopup()
+	icons_parent:SetKeyboardInputEnabled(false)
+	icons_parent:Center()
+
+	local function CreateIconFrame(x, y, w, h, slot)
+		local icon = vgui.Create("Panel", icons_parent)
+		icon:SetPos(x, y)
+		icon:SetSize(w, h)
+		icon.SlotIndex = slot
+
+		local offset = math.floor(3 * menuscale)
+
+		function icon:Paint(w, h)
+			surface.SetDrawColor(255, 255, 255)
+
+			surface.SetMaterial(menuwhite)
+			surface.DrawTexturedRect(0, 0, w, h)
+
+			surface.SetMaterial(menublack)
+			surface.DrawTexturedRect(offset, offset, w - offset * 2, h - offset * 2)
+
+			local class = inventory[self.SlotIndex]
+
+			if class then
+				local active_weapon = local_player:GetActiveWeapon()
+
+				if active_weapon:IsValid() and active_weapon:GetClass() == class then
+					DisableClipping(true)
+						surface.SetDrawColor(200, 200, 200)
+						surface.DrawOutlinedRect(-3, -3, w + 6, h + 6, 3)
+					DisableClipping(false)
+				end
+			end
+
+			if self:IsHovered() or self:IsChildHovered() then
+				DisableClipping(true)
+					surface.SetDrawColor(255, 0, 0)
+					surface.DrawOutlinedRect(-1, -1, w + 2, h + 2, 1)
+
+					local class = inventory[self.SlotIndex]
+
+					if class then
+						draw.SimpleText(GetClassName(class), "SCPCB_Inventory", w / 2, h + spacing / 1.5, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, color_black)
+					end
+				DisableClipping(false)
+			end
+
+			return true
+		end
+
+		local image = vgui.Create("DPanel", icon)
+		image:SetPos(offset, offset)
+		image:SetSize(w - offset * 2, h - offset * 2)
+		image:SetMouseInputEnabled(true)
+		image:Droppable("scpcb_slot")
+		image.SlotIndex = slot
+
+		function image:Paint(w, h)
+			if not inventory[self.SlotIndex] then return end
+			local class_icon = GetClassIcon(inventory[self.SlotIndex])
+
+			surface.SetDrawColor(255, 255, 255)
+			surface.SetMaterial(Material(class_icon, "smooth"))
+			surface.DrawTexturedRect(0, 0, w, h)
+
+			return true
+		end
+
+		-- Dragdrop support
+		local OnMousePressed = image.OnMousePressed
+
+		function image:OnMousePressed(key)
+			local last_click = self.LastClick
+			OnMousePressed(self, key)
+
+			if not last_click then
+				self.LastClick = SysTime()
+				return
+			end
+
+			if SysTime() - last_click > 0.3 then
+				self.LastClick = SysTime()
+				return
+			end
+
+			net.Start("SCPCB_Inventory")
+			net.WriteUInt(2, 3)
+			net.WriteUInt(self.SlotIndex, 4)
+			net.SendToServer()
+		end
+
+		image:Receiver("scpcb_slot", function(receiver, tab, fully_dropped)
+			if not fully_dropped then
+				return
+			end
+
+			local dropped = tab[1]
+			local receiver_slot = receiver.SlotIndex
+			local dropped_slot = dropped.SlotIndex
+
+			if inventory[receiver_slot] or not inventory[dropped_slot] then
+				return
+			end
+
+			net.Start("SCPCB_Inventory")
+			net.WriteUInt(3, 3)
+			net.WriteUInt(dropped_slot, 4)
+			net.WriteUInt(receiver_slot, 4)
+			net.SendToServer()
+
+			inventory[receiver_slot] = inventory[dropped_slot]
+			inventory[dropped_slot] = nil
+		end)
+	end
+
+	local world_panel = vgui.GetWorldPanel()
+
+	world_panel:Receiver("scpcb_slot", function(receiver, tab, fully_dropped)
+		if not fully_dropped then
+			return
+		end
+
+		local dropped = tab[1]
+		local dropped_slot = dropped.SlotIndex
+
+		if not inventory[dropped_slot] then
+			return
+		end
+
+		net.Start("SCPCB_Inventory")
+		net.WriteUInt(1, 3)
+		net.WriteUInt(dropped_slot, 4)
+		net.SendToServer()
+
+		surface.PlaySound("scpcb/pickitem2.ogg")
+		inventory[dropped_slot] = nil
+	end)
+
+	for row = 1, rows do
+		for col = 1, cols do
+			local x = (col - 1) * (slot_size + spacing)
+			local y = (row - 1) * (slot_size * 2)
+			CreateIconFrame(x, y, slot_size, slot_size, (row - 1) * cols + col)
+		end
+	end
+end)
+
+net.Receive("SCPCB_Inventory", function()
+	inventory[net.ReadUInt(4)] = net.ReadString()
+end)
+
+net.Receive("SCPCB_ClearInventory", function()
+	inventory = {}
+end)
---
+++ lua/autorun/server/sv_scpcb_inv.lua
@@ -0,0 +1,139 @@
+util.AddNetworkString("SCPCB_Inventory")
+util.AddNetworkString("SCPCB_ClearInventory")
+
+local function FindFreeSlot(inventory)
+	for i = 1, 10 do
+		if not inventory[i] then
+			return i
+		end
+	end
+end
+
+local function SetupInventory(ply)
+	local inventory = ply.SCPCBItems
+
+	if not inventory then
+		inventory = {}
+		ply.SCPCBItems = inventory
+	end
+
+	return inventory
+end
+
+local function SendSlotChange(slot, class, ply)
+	net.Start("SCPCB_Inventory")
+	net.WriteUInt(slot, 4)
+	net.WriteString(class)
+	net.Send(ply)
+end
+
+local ignore_pickupcheck
+local ignore_soundcheck
+
+net.Receive("SCPCB_Inventory", function(len, ply)
+	local inventory = SetupInventory(ply)
+	local action = net.ReadUInt(3)
+
+	-- Drop
+	if action == 1 then
+		local dropped_slot = net.ReadUInt(4)
+		local item = inventory[dropped_slot]
+
+		if item then
+			if item.is_weapon then
+				ignore_pickupcheck = true
+
+				ply:Give(item.class, true)
+				ply:DropNamedWeapon(item.class)
+
+				ignore_pickupcheck = false
+			end
+		end
+
+		inventory[dropped_slot] = nil
+	end
+
+	-- Use
+	if action == 2 then
+		local used_slot = net.ReadUInt(4)
+		local item = inventory[used_slot]
+
+		if item then
+			if item.is_weapon then
+				local active_weapon = ply:GetActiveWeapon()
+
+				if active_weapon:IsValid() and active_weapon:GetClass() == item.class then
+					ply:StripWeapons()
+				else
+					ignore_pickupcheck = true
+
+					ply:StripWeapons()
+					ply:Give(item.class, item.ammo_given)
+					item.ammo_given = true
+
+					ignore_pickupcheck = false
+				end
+
+				ply:EmitSound("scpcb/pickitem2.ogg")
+			end
+		end
+	end
+
+	-- Move
+	if action == 3 then
+		local dropped_slot = net.ReadUInt(4)
+		local receiver_slot = net.ReadUInt(4)
+
+		if inventory[receiver_slot] or not inventory[dropped_slot] then
+			return
+		end
+
+		inventory[receiver_slot] = inventory[dropped_slot]
+		inventory[dropped_slot] = nil
+	end
+end)
+
+hook.Add("PlayerCanPickupWeapon", "SCPCB_PickupWeapon", function(ply, weapon)
+	if ignore_pickupcheck or weapon:IsMarkedForDeletion() then
+		return
+	end
+
+	local inventory = SetupInventory(ply)
+	local slot = FindFreeSlot(inventory)
+
+	if slot then
+		local class = weapon:GetClass()
+		inventory[slot] = {class = class, is_weapon = true}
+		SendSlotChange(slot, class, ply)
+
+		if not ignore_soundcheck then
+			ply:EmitSound("scpcb/pickitem2.ogg")
+		end
+
+		weapon:Remove()
+	end
+
+	return false
+end)
+
+hook.Add("PlayerSwitchWeapon", "SCPCB_SwitchWeaponDisallow", function()
+	if ignore_pickupcheck then
+		return
+	end
+
+	return true
+end)
+
+hook.Add("PlayerSpawn", "SCPCB_SilentSpawnEquip", function()
+	ignore_soundcheck = true
+end)
+
+hook.Add("PlayerSetModel", "SCPCB_SilentSpawnEquip", function()
+	ignore_soundcheck = false
+end)
+
+hook.Add("PostPlayerDeath", "SCPCB_ClearInventory", function(ply)
+	ply.SCPCBItems = {}
+	net.Start("SCPCB_ClearInventory")
+	net.Send(ply)
+end)
